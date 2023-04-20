abc06f6222a61b738a9e22c77073a73cf658da93
Ayush Singh <aashu.ayush0003@gmail.com>
2023-04-20 15:45:46 +0530

    Making some changes in the functionality of the site and using react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes
-// } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
  
 function App() {
@@ -30,7 +30,7 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-      document.title = 'TextUtils - Dark Mode';
+      // document.title = 'TextUtils - Dark Mode';
       // setInterval(() => {
       //   document.title = 'TextUtils is Amazing Mode';
       // }, 2000);
@@ -42,30 +42,31 @@ function App() {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      document.title = 'TextUtils - Light Mode';
+      // document.title = 'TextUtils - Light Mode';
     }
   }
   return (
     <>
     {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
     {/* <Navbar/> */}
-    {/* <Router> */}
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
     <Alert alert={alert}/>
     <div className="container my-3">
-    {/* <Routes> */}
+    <Routes>
     {/* /users --> Component 1
         /users/home --> Component 2 */}
-          {/* <Route exact path="/about" element={<About />}>
+          <Route exact path="/about" element={<About mode={mode} />}>
   
-          </Route> */}
-          {/* <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}>
+          </Route> 
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="TextUtils - Word Counter, Character Counter, 
+          Remove Extra Spaces" mode={mode}/>}>
             
-          </Route> */}
-    {/* </Routes> */}
-    <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
+          </Route>
+     </Routes>
+    {/* // <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/> */}
     </div>
-    {/* </Router> */}
+    </Router> 
     </> 
   );
 }
